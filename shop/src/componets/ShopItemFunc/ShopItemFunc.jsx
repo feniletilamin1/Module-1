e49468c8a41768c4231f418f6220a53ea49b0383
feniletilamin <tsibutsinini.serezhaa@yandex.ru>
2023-07-25 11:05:47 +0300

    Initialize commit message

---
+++ shop/src/componets/ShopItemFunc/ShopItemFunc.jsx
@@ -0,0 +1,22 @@
+
+const ShopItemFunc = (props) => {
+    const { item } = props;
+    return (
+        <div class="main-content">
+            <h2>{item.title}</h2>
+            <h1>{item.brand}</h1>
+            <h3>{item.description}</h3>
+            <div class="description">
+                {item.descriptionFull}
+            </div>
+            <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+            <div class="divider"></div>
+            <div class="purchase-info">
+                <div class="price">{item.currency}{item.price.toFixed(2)}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    )
+}
+
+export default ShopItemFunc
