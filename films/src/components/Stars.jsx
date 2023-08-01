68e3725447440564a01ebbbf819f1705c0227805
feniletilamin <tsibutsinini.serezhaa@yandex.ru>
2023-08-01 11:50:45 +0300

    Изменено задание с фильмами

--- films/src/components/Stars.jsx
+++ films/src/components/Stars.jsx
@@ -1,3 +1,5 @@
+import Star from "./Star";
+
 export default function Stars(props) {
     const { count } = props ?? 0;
 
@@ -6,14 +8,9 @@ export default function Stars(props) {
     const arr = Array.from({ length: count }, (_, index) => index);
 
     return (
-        <ul class="card-body-stars u-clearfix">
+        <ul className="card-body-stars u-clearfix">
             {arr.map((star, index) =>
-                <li key={index}>
-                    <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" />
-                        <path d="M0 0h18v18H0z" fill="none" />
-                    </svg>
-                </li>
+                <Star key={index} />
             )}
         </ul>
     )
